07b7512fd32bce61e813deaf014db2ace7c92b2c
etiennewayne <etiennewaye@gmail.com>
2020-10-21 17:29:34 +0800

    combine android and webroute update

---
+++ LeQuizApp/app/src/main/java/com/example/alab/lequizapp/CategoryAddUpdate.java
@@ -0,0 +1,14 @@
+package com.example.alab.lequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryAddUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_add_update);
+    }
+}
---
+++ LeQuizApp/app/src/main/java/com/example/alab/lequizapp/CategoryMain.java
@@ -0,0 +1,14 @@
+package com.example.alab.lequizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_main);
+    }
+}
