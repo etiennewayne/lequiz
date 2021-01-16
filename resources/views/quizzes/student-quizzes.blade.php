23fd05f63a7a2078614ec0ea8fc420ee9775ac66
etiennewayne <etiennewaye@gmail.com>
2021-01-16 13:26:16 +0800

    update lequiz import/export

--- resources/views/quizzes/student-quizzes.blade.php
+++ resources/views/quizzes/student-quizzes.blade.php
@@ -85,21 +85,32 @@
 
     <hr>
 
-    <a href="" class="btn btn-warning mb-2">Export</a>
-
-
 
 </div> <!-- close container div-->
 
 @endsection
 
 @section('extrascript')
-	<script type="text/javascript">
+
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+  
+  <script type="text/javascript">
 
 		$(document).ready(function(){
 
             var table = $('#questions').DataTable({
                 processing: true,
+                dom: 'Bfrtip',
+                buttons: [
+                  'copy', 'csv', 'excel', 'pdf', 'print'
+                ],
                 ajax: {
                     url: '/quiz/student-quizzes/ajax/studentquiz/{{ $quizid }}',
                     dataSrc: ''
